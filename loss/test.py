f098426acdcffdbe2cd82632f6de927c5ec0f530
kimnamhyeok <dr_lunars@naver.com>
2020-10-28 14:54:14 +0900

    Word2Vec

---
+++ loss/test.py
@@ -0,0 +1,10 @@
+from common.np import *
+
+def test(y, t):
+    if y.ndim == 1:
+        t = t.reshape(1, t.size)
+        y = y.reshape(1, y.size)
+    if t.size == y.size:
+        t = t.argmax(axis=1)
+    batch_size = y.shape[0]
+    return np.nan
