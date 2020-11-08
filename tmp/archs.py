851562b89081b0dd49725276bedd2c54315ca3ba
kimnamhyeok <dr_lunars@naver.com>
2020-11-09 00:27:02 +0900

    Word2Vec

---
+++ tmp/archs.py
@@ -0,0 +1,41 @@
+from keras.models import Model
+from keras.layers import Dense, Conv2D, BatchNormalization, Activation
+from keras.layers import Input, MaxPooling2D, Dropout, Flatten
+from metrics import *
+
+weight_decay = 1e-4
+
+def w2v(args):
+    input = Input(shape=(28, 28, 1))
+    x = input
+    x = Flatten(input_shape=(28, 28, 1))(x)
+    x = Dense(args.num_features, kernel_initializer='he_normal', kernel_regularizer=regularizers.l2(weight_decay))(x)
+    output = Dense(10, activation='softmax', kernel_regularizer=regularizers.l2(weight_decay))(x)
+    return Model(input, output)
+
+def w2v_arcface(args):
+    input = Input(shape=(28, 28, 1))
+    y = Input(shape=(10,))
+    x = input
+    x = Flatten(input_shape=(28, 28, 1))(x)
+    x = Dense(args.num_features, kernel_initializer='he_normal', kernel_regularizer=regularizers.l2(weight_decay))(x)
+    output = ArcFace(10, regularizer=regularizers.l2(weight_decay))([x, y])
+    return Model([input, y], output)
+
+def w2v_cosface(args):
+    input = Input(shape=(28, 28, 1))
+    y = Input(shape=(10,))
+    x = input
+    x = Flatten(input_shape=(28, 28, 1))(x)
+    x = Dense(args.num_features, kernel_initializer='he_normal', kernel_regularizer=regularizers.l2(weight_decay))(x)
+    output = CosFace(10, regularizer=regularizers.l2(weight_decay))([x, y])
+    return Model([input, y], output)
+
+def w2v_sphereface(args):
+    input = Input(shape=(28, 28, 1))
+    y = Input(shape=(10,))
+    x = input
+    x = Flatten(input_shape=(28, 28, 1))(x)
+    x = Dense(args.num_features, kernel_initializer='he_normal', kernel_regularizer=regularizers.l2(weight_decay))(x)
+    output = SphereFace(10, regularizer=regularizers.l2(weight_decay))([x, y])
+    return Model([input, y], output)
